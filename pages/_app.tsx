f70cff222b13213fdd130cdb11774e57bb67e7fa
lionel <lionel.rouge@godecision.fr>
2022-11-10 10:58:10 +0100

    chore: add the auth0 sdk

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,17 @@ import "../styles/tailwind.css";
 import Layout from "../components/Layout";
 import { ApolloProvider } from "@apollo/client";
 import apolloClient from "../lib/apollo";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ApolloProvider client={apolloClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ApolloProvider>
+    <UserProvider>
+      <ApolloProvider client={apolloClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
+    </UserProvider>
   );
 }
 
@@ -18,3 +21,5 @@ export default MyApp;
 // wrapping the global App component with the Apollo Provider so all of the project's components can send GraphQL queries.
 
 // Note: Next.js supports different data fetching strategies. You can fetch data server-side, client-side, or at build-time. To support pagination, you need to fetch data client-side.
+
+//Wrapping the MyApp component with the UserProvider component will allow all pages to access your user's authentication state.
---
+++ pages/api/[...auth0].ts
@@ -0,0 +1,10 @@
+import { handleAuth } from "@auth0/nextjs-auth0";
+
+export default handleAuth();
+
+// This Next.js dynamic API route will automatically create the following endpoints:
+
+// /api/auth/login: Auth0's login route.
+// /api/auth/logout: The route used to logout the user.
+// /api/auth/callback: The route Auth0 redirects the user to after a successful login.
+// /api/auth/me: The route to fetch the user profile from Auth0.
