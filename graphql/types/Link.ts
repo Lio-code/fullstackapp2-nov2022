91a12e306a87e8d54fbb35f111b7e7a73c0598cc
lionel <lionel.rouge@godecision.fr>
2022-11-10 18:17:31 +0100

    feat: create link

--- graphql/types/Link.ts
+++ graphql/types/Link.ts
@@ -1,5 +1,14 @@
-import { objectType, extendType } from "nexus";
+import { objectType, extendType, stringArg, nonNull } from "nexus";
 import { User } from "./User";
+
+// the extendType() function from Nexus to create a query.
+
+//- nonNull specifies that clients will always get a value for this field. In Nexus, all "output types" (types returned by fields) are nullable by default.
+//- list specifies that this query will return a list.
+//
+//field() is a function that takes two arguments:
+//- The field's name. In this case, the query is called "links" like the schema you created initially.
+//- An object that specifies the returned type of the query along with the resolver function. In the resolver function, you are accessing prisma from the context and using the findMany() function to return all links in the Link table of the database.
 
 export const Link = objectType({
   name: "Link",
@@ -37,11 +46,37 @@ export const LinksQuery = extendType({
   },
 });
 
-// the extendType() function from Nexus to create a query.
+// The args property defines the input required to create a new link. The mutation also checks if a user is logged in so only authenticated users can create links. Finally, the create() function from Prisma creates a new database record.
 
-//- nonNull specifies that clients will always get a value for this field. In Nexus, all "output types" (types returned by fields) are nullable by default.
-//- list specifies that this query will return a list.
-//
-//field() is a function that takes two arguments:
-//- The field's name. In this case, the query is called "links" like the schema you created initially.
-//- An object that specifies the returned type of the query along with the resolver function. In the resolver function, you are accessing prisma from the context and using the findMany() function to return all links in the Link table of the database.
+export const CreateLinkMutation = extendType({
+  type: "Mutation",
+  definition(t) {
+    t.nonNull.field("createLink", {
+      type: Link,
+      args: {
+        title: nonNull(stringArg()),
+        url: nonNull(stringArg()),
+        imageUrl: nonNull(stringArg()),
+        category: nonNull(stringArg()),
+        description: nonNull(stringArg()),
+      },
+      async resolve(_parent, args, ctx) {
+        if (!ctx.user) {
+          throw new Error(`You need to be logged in to perform an action`);
+        }
+
+        const newLink = {
+          title: args.title,
+          url: args.url,
+          imageUrl: args.imageUrl,
+          category: args.category,
+          description: args.description,
+        };
+
+        return await ctx.prisma.link.create({
+          data: newLink,
+        });
+      },
+    });
+  },
+});
